3c7c522e786116c8e3bdd08ed44dcd8c790fc720
Marina Marinova <marina.marinova@mentormate.com>
2017-06-30 19:46:14 +0300

    Initial commit

---
+++ wp-content/themes/workshop/functions.php
@@ -0,0 +1,4 @@
+<?php
+
+$HOME_DIR = get_template_directory_uri();
+define(HOME_DIR, $HOME_DIR);
---
+++ wp-content/themes/workshop/header.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <base href="<?php echo HOME_DIR; ?>/" >
+        <!--
+
+Template 2082 Pure Mix
+
+http://www.tooplate.com/view/2082-pure-mix
+
+        -->
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="keywords" content="">
+        <meta name="description" content="">
+
+        <!-- Site title
+   ================================================== -->
+        <title>Pure Mix - HTML CSS Template</title>
+
+        <!-- Bootstrap CSS
+   ================================================== -->
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+
+        <!-- Animate CSS
+   ================================================== -->
+        <link rel="stylesheet" href="css/animate.min.css">
+
+        <!-- Font Icons CSS
+   ================================================== -->
+        <link rel="stylesheet" href="css/font-awesome.min.css">
+        <link rel="stylesheet" href="css/ionicons.min.css">
+
+        <!-- Main CSS
+   ================================================== -->
+        <link rel="stylesheet" href="css/style.css">
+
+        <!-- Google web font
+   ================================================== -->	
+        <link href='https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700,300' rel='stylesheet' type='text/css'>
+
+    </head>
